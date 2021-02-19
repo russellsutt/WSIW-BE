339b494f87dabf0146a0cecd934cf4c52f7f1407
russellsutt <russell.sutter@gmail.com>
2021-02-19 13:12:43 -0800

    Setting up DB

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts');
+});
+
+app.listen(3000);
